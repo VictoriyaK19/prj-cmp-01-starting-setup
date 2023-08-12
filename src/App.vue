171a17508b70bf9ee38b6275c7bcee7aaf92c085
VictoriyaK19 <kalinovavictoriya@gmail.com>
2023-08-12 17:37:03 +0300

    Dynamic Components & Attribute Fallthrough

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
     <the-header title="RememberMe"></the-header>
-    <stored-resources :resources="storedResources"></stored-resources>
-</template>
+    </template>
 
 <script>
-import StoredResources from "./components/learning-resources/StoredResources.vue";
 import TheHeader from '@/components/layouts/TheHeader';
 
 export default {
   components: {
-    StoredResources,
     TheHeader
   },
   data() {
