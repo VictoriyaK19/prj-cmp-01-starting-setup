5dfe125a14b1dd71568fd5b43cbefc29e492b1c8
VictoriyaK19 <kalinovavictoriya@gmail.com>
2023-08-12 15:56:26 +0300

    First Components & Props

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
   <ul>
-    <li></li>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link">
+    ></learning-resource>
   </ul>
 </template>
 
 <script>
-import { VueElement } from "vue";
+import LearningResource from "@/components/learning-resources/LearningResource";
 
 export default {
+  components: {
+    LearningResource,
+  },
   data() {
     return {
-      storedResourses: [
+      storedResources: [
         {
           id: "official-guide",
           title: "Official Guide",
           description: "The official VueElement.js documentation.",
-          link: 'https://vuejs.org'
+          link: "https://vuejs.org",
         },
         {
           id: "google",
           title: "Google",
           description: "Learn to google",
-          link: 'https://google.org'
+          link: "https://google.org",
         },
       ],
     };
